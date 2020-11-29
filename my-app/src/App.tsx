51ebc5b6aa6a6099860ec0c6afb80333ca52f7b7
BuckUbel <webquentin@web.de>
2020-11-29 23:29:52 +0100

    Extract to separate components and update dependencies

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,33 +1,13 @@
 import React from 'react';
 import './App.css';
-import {useKeyBoard} from "./useKeyBoard";
-import {useTextCalculator} from "./useTextCalculator";
-
+import NameValidatorComponent from "./nameValidator/NameValidatorComponent";
 
 function App() {
-  const [userText, caretPos] = useKeyBoard('', 0);
-  const textValue = useTextCalculator(userText);
-
   return (
     <div className="AppContainer">
       <div className="App">
         <h2>Name Validator</h2>
-        <h5>Try it and input your name {textValue > 0 && textValue}
-        </h5>
-        <div id={"the-input-field-container"}>
-          <div id={"the-input-field"} className={"typewriter"}>
-            <input id={"the-hidden-real-input-field"} autoFocus/>
-            <p>
-              {caretPos === 0 && <span id={"the-caret"}/>}
-              {Array.from(userText).map((l: string, i: number) => {
-                return <React.Fragment key={i}>
-                  {l}
-                  {(i === caretPos - 1) && <span id={"the-caret"}/>}
-                </React.Fragment>;
-              })}
-            </p>
-          </div>
-        </div>
+        <NameValidatorComponent/>
       </div>
     </div>
   );
