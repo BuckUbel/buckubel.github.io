4d767712a42fbcf61daafefa179645f13dd360b2
BuckUbel <webquentin@web.de>
2020-11-29 23:40:07 +0100

    Implemented styled-components

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import NameValidatorComponent from "./nameValidator/NameValidatorComponent";
+import NameValidator from "./nameValidator/NameValidator";
 
 function App() {
   return (
     <div className="AppContainer">
       <div className="App">
         <h2>Name Validator</h2>
-        <NameValidatorComponent/>
+        <NameValidator/>
       </div>
     </div>
   );
---
+++ my-app/src/nameValidator/NameValidator.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import {useKeyBoard} from "./useKeyBoard";
+import {useTextCalculator} from "./useTextCalculator";
+import styled from 'styled-components';
+
+interface NameValidatorProps {
+  className?: string;
+}
+
+function NameValidator(props: NameValidatorProps) {
+  const [userText, caretPos] = useKeyBoard('', 0);
+  const textValue = useTextCalculator(userText);
+
+  return (
+    <div className={props.className}>
+      <h5>Try it and input your name {textValue > 0 && textValue}</h5>
+      <div id={"the-input-field-container"}>
+        <div id={"the-input-field"} className={"typewriter"}>
+          <input id={"the-hidden-real-input-field"} autoFocus/>
+          <p>
+            {caretPos === 0 && <span id={"the-caret"}/>}
+            {Array.from(userText).map((l: string, i: number) => {
+              return <React.Fragment key={i}>
+                {l}
+                {(i === caretPos - 1) && <span id={"the-caret"}/>}
+              </React.Fragment>;
+            })}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default styled(NameValidator)`
+
+#the-input-field-container {
+    background: #000;
+    border: 1px #fff solid;
+    border-radius: 15px;
+}
+
+#the-hidden-real-input-field {
+    display: none;
+}
+
+#the-input-field {
+    background: #000;
+    color: #fa0;
+    text-shadow: 0 0 20px #fa0;
+    max-width: 92%;
+    margin: 0 auto;
+    min-height: 38px;
+    overflow: hidden;
+}
+
+.typewriter #the-caret {
+    overflow: visible;
+    border-right: .1em solid #fff;
+    white-space: nowrap;
+    margin: 0 auto;
+    letter-spacing: .15em;
+    animation: blink-caret 0.75s step-end infinite;
+    height: 38px;
+}
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+    from, to {
+        border-color: transparent
+    }
+    50% {
+        border-color: #fff;
+    }
+}
+`;
