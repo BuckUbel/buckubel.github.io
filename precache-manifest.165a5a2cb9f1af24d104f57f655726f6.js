c989384a2f6a38f135492de7a1726d8fa7187c59
BuckUbel <webquentin@web.de>
2020-12-01 02:01:44 +0100

    Intial dist subtree commit

---
+++ precache-manifest.165a5a2cb9f1af24d104f57f655726f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ae2c0cf06d70f333a574331a717d02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74af3f27639492716c8b",
+    "url": "/static/css/main.b772d0cf.chunk.css"
+  },
+  {
+    "revision": "3851e4ca9712453b6ee5",
+    "url": "/static/js/2.33a623bd.chunk.js"
+  },
+  {
+    "revision": "4c7a7372b304d58db258d12cbd493f5c",
+    "url": "/static/js/2.33a623bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74af3f27639492716c8b",
+    "url": "/static/js/main.0a6c142c.chunk.js"
+  },
+  {
+    "revision": "5cf7ce7312b6ca4de309",
+    "url": "/static/js/runtime-main.327b621f.js"
+  },
+  {
+    "revision": "e132f523789e74cf96e3e3e212de4392",
+    "url": "/static/media/banner1024.e132f523.png"
+  }
+]);
