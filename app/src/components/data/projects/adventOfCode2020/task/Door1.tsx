2ca8355aee6a851792bcb72ab75339c5c8098ed1
BuckUbel <webquentin@web.de>
2020-12-02 02:12:15 +0100

    Add time information to door 1

--- app/src/components/data/projects/adventOfCode2020/task/Door1.tsx
+++ app/src/components/data/projects/adventOfCode2020/task/Door1.tsx
@@ -7,6 +7,7 @@ interface Door1Props extends StyledCompProps {
 
 function Door1(props: Door1Props) {
 
+  const stepOne = window.performance.now();
   const target = 2020;
 
   const startInput = [1825, 1944, 1802, 1676, 1921, 1652, 1710, 1952, 1932, 1934, 1823, 1732, 1795, 1681, 1706, 1697, 1919, 1695, 2007, 1889, 1942, 961, 1868, 1878, 1723, 416, 1875, 1831, 1890, 1654, 1956, 1827, 973, 1947, 1688, 1680, 1808, 1998, 1794, 1552, 1935, 1693, 1824, 1711, 1766, 1668, 1968, 1884, 217, 2003, 1869, 1658, 1953, 1829, 1984, 2005, 1973, 428, 1957, 1925, 1719, 1797, 321, 1804, 1971, 922, 1976, 1863, 2008, 1806, 1833, 1809, 1707, 1954, 1811, 1815, 1915, 1799, 1917, 1664, 1937, 1775, 1685, 1756, 1940, 1660, 1859, 1916, 1989, 1763, 1994, 1716, 1689, 1866, 1708, 1670, 1982, 1870, 1847, 1627, 1819, 1786, 1828, 1640, 1699, 1722, 1737, 1882, 1666, 1871, 1703, 1770, 1623, 1837, 1636, 1655, 1930, 1739, 1810, 1805, 1861, 1922, 1993, 1896, 1760, 2002, 1779, 1633, 1972, 1856, 1641, 1718, 2004, 1730, 1826, 1923, 1753, 1735, 660, 1988, 1796, 1990, 1720, 1626, 1788, 1700, 942, 1902, 1943, 1758, 1839, 1924, 938, 1634, 1724, 1983, 1683, 1687, 1904, 1907, 1757, 2001, 1910, 1849, 1781, 1981, 1743, 1851, 2009, 619, 1898, 1891, 1751, 1765, 1959, 1888, 1894, 1759, 389, 1964, 1900, 1742, 1672, 1969, 1978, 1933, 1906, 1807, 1867, 1838, 1960, 1814, 1950, 1918, 1726, 1986, 1746, 2006, 1949, 1784];
@@ -34,6 +35,7 @@ function Door1(props: Door1Props) {
       }
     }
   }
+  const stepTwo = window.performance.now();
 
   const filteredInput2 = filteredInput.filter((v) => v < (target - filteredInput[0] - filteredInput[1]))
   let foundedNumber2One = -1;
@@ -56,7 +58,7 @@ function Door1(props: Door1Props) {
       }
     }
   }
-
+  const stepThree = window.performance.now();
 
   return (
     <div className={props.className}>
@@ -108,18 +110,27 @@ function Door1(props: Door1Props) {
 
       <p><b>Endergebnis: </b> Um jetzt diese Lösung zu bewerten können wir uns die Schleifendurchgänge anschauen:</p>
       <ul>
-        <li>{sortIterations} - Sortierung </li>
+        <li>{sortIterations} - Sortierung</li>
         <li>{sortedInput.length} - 1. Filterung (Länge des Arrays)</li>
-        <li>{iterations} - 1. Algorithmus </li>
+        <li>{iterations} - 1. Algorithmus</li>
         <li>{filteredInput.length} - 2. Filterung (Länge des zuerst gefilterten Arrays)</li>
-        <li>{iterations2} - 1. Algorithmus </li>
+        <li>{iterations2} - 1. Algorithmus</li>
       </ul>
       <p>Das macht am Ende zusammen <b>{sortIterations +
       sortedInput.length +
       iterations +
       filteredInput.length +
-      iterations2} Durchgänge</b></p>
-      <p> Ich bin gespannt schaffst Du weniger? Probiere es doch aus vielleicht findest du ja einen besseren Algorithmus ;)</p>
+      iterations2} Durchgänge.</b></p>
+
+      <p> Eine weitere Beurteilung gelingt uns durch die Zeit. Unterteil in die beiden Bereiche sind folgende
+        Zeitabstände entstanden (Bitte bedenke, dass diese Zeiten live berechnet werden, d.h. sie sind schneler umso
+        stärker dein Endgerät ist):</p>
+      <ul>
+        <li>{((stepTwo - stepOne) * 1000).toFixed(2)} μs (mein Endgerät lag bei 100)</li>
+        <li>{((stepThree - stepTwo) * 1000).toFixed(2)} μs (mein Endgerät lag bei 20)</li>
+      </ul>
+      <p> Ich bin gespannt schaffst Du weniger? Probiere es doch aus vielleicht findest du ja einen besseren Algorithmus
+        ;)</p>
       <hr/>
       <hr/>
     </div>
