f7924932f813ec15cfd8e519cc3ed8a195875c8e
BuckUbel <webquentin52@gmail.com>
2021-04-05 21:20:23 +0200

    Remove not used doors

--- app/src/components/data/projects/adventOfCode2020/DoorContent.ts
+++ app/src/components/data/projects/adventOfCode2020/DoorContent.ts
@@ -3,25 +3,6 @@ import data2 from "./data/2.json";
 import data3 from "./data/3.json";
 import data4 from "./data/4.json";
 import data5 from "./data/5.json";
-import data6 from "./data/6.json";
-import data7 from "./data/7.json";
-import data8 from "./data/8.json";
-import data9 from "./data/9.json";
-import data10 from "./data/10.json";
-import data11 from "./data/11.json";
-import data12 from "./data/12.json";
-import data13 from "./data/13.json";
-import data14 from "./data/14.json";
-import data15 from "./data/15.json";
-import data16 from "./data/16.json";
-import data17 from "./data/17.json";
-import data18 from "./data/18.json";
-import data19 from "./data/19.json";
-import data20 from "./data/20.json";
-import data21 from "./data/21.json";
-import data22 from "./data/22.json";
-import data23 from "./data/23.json";
-import data24 from "./data/24.json";
 
 const DOOR_DATA = [
   data1,
@@ -29,25 +10,6 @@ const DOOR_DATA = [
   data3,
   data4,
   data5,
-  data6,
-  data7,
-  data8,
-  data9,
-  data10,
-  data11,
-  data12,
-  data13,
-  data14,
-  data15,
-  data16,
-  data17,
-  data18,
-  data19,
-  data20,
-  data21,
-  data22,
-  data23,
-  data24
 ]
 
 export interface DoorContentInterface {
--- app/src/components/data/projects/adventOfCode2020/Doors.tsx
+++ app/src/components/data/projects/adventOfCode2020/Doors.tsx
@@ -6,28 +6,9 @@ import Door1 from "./task/Door1";
 import DoorItem from "./DoorItem";
 import {Color} from "../../../config/color";
 import Door2 from "./task/Door2";
-import Door24 from "./task/Door24";
-import Door23 from "./task/Door23";
-import Door22 from "./task/Door22";
-import Door21 from "./task/Door21";
-import Door20 from "./task/Door20";
-import Door19 from "./task/Door19";
-import Door18 from "./task/Door18";
-import Door17 from "./task/Door17";
-import Door16 from "./task/Door16";
-import Door15 from "./task/Door15";
-import Door14 from "./task/Door14";
-import Door13 from "./task/Door13";
-import Door10 from "./task/Door10";
-import Door11 from "./task/Door11";
-import Door12 from "./task/Door12";
-import Door4 from "./task/Door4";
 import Door3 from "./task/Door3";
+import Door4 from "./task/Door4";
 import Door5 from "./task/Door5";
-import Door6 from "./task/Door6";
-import Door7 from "./task/Door7";
-import Door8 from "./task/Door8";
-import Door9 from "./task/Door9";
 
 interface DoorsProps extends StyledCompProps {
   id: number
@@ -56,63 +37,6 @@ function Doors(props: DoorsProps) {
       break;
     case 5:
       doorComp = <Door5/>;
-      break;
-    case 6:
-      doorComp = <Door6/>;
-      break;
-    case 7:
-      doorComp = <Door7/>;
-      break;
-    case 8:
-      doorComp = <Door8/>;
-      break;
-    case 9:
-      doorComp = <Door9/>;
-      break;
-    case 10:
-      doorComp = <Door10/>;
-      break;
-    case 11:
-      doorComp = <Door11/>;
-      break;
-    case 12:
-      doorComp = <Door12/>;
-      break;
-    case 13:
-      doorComp = <Door13/>;
-      break;
-    case 14:
-      doorComp = <Door14/>;
-      break;
-    case 15:
-      doorComp = <Door15/>;
-      break;
-    case 16:
-      doorComp = <Door16/>;
-      break;
-    case 17:
-      doorComp = <Door17/>;
-      break;
-    case 18:
-      doorComp = <Door18/>;
-      break;
-    case 19:
-      doorComp = <Door19/>;
-      break;
-    case 20:
-      doorComp = <Door20/>;
-      break;
-    case 21:
-      doorComp = <Door21/>;
-      break;
-    case 22:
-      doorComp = <Door22/>;
-      break;
-    case 23:
-      doorComp = <Door23/>;
-      break;
-    case 24:
-      doorComp = <Door24/>;
       break;
   }
 
