c19b318476930487ac33f09586d962f4230ec73b
BuckUbel <webquentin52@gmail.com>
2021-05-21 23:41:16 +0200

    Add createdAt and tags infos for blog-entries

---
+++ precache-manifest.c2ee5cb21dd2668faf3421a51db155c2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daeba56a42178231909e2561b4aa4d02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c32f973c491aa43daf3",
+    "url": "/static/css/main.b500b906.chunk.css"
+  },
+  {
+    "revision": "ebe9faf0681298f107f4",
+    "url": "/static/js/2.c2e8ffff.chunk.js"
+  },
+  {
+    "revision": "c87394bd40c39961abd737af90139a4f",
+    "url": "/static/js/2.c2e8ffff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c32f973c491aa43daf3",
+    "url": "/static/js/main.a0f5e04e.chunk.js"
+  },
+  {
+    "revision": "5cf7ce7312b6ca4de309",
+    "url": "/static/js/runtime-main.327b621f.js"
+  },
+  {
+    "revision": "ca3cffd996252ba14a3f9a11a0becf9f",
+    "url": "/static/media/BlogEntry0.ca3cffd9.md"
+  },
+  {
+    "revision": "0b3068c5e04d8c86ab34e35c748eb058",
+    "url": "/static/media/BlogEntry1.0b3068c5.md"
+  },
+  {
+    "revision": "e132f523789e74cf96e3e3e212de4392",
+    "url": "/static/media/banner1024.e132f523.png"
+  }
+]);
---
+++ static/media/BlogEntry1.0b3068c5.md
@@ -0,0 +1,6 @@
+BLOG_TAGS: Blog, MediaMaster
+BLOG_CREATED_AT: 30.05.2021
+
+# Über Blogs und der MediaMaster
+
+Bla bla bla
