f24943641079fa27d70518f6a7a1d124df668ed7
BuckUbel <webquentin@web.de>
2022-05-27 23:15:36 +0200

    Split InputField into EntityInputField and a default one

--- app/src/components/data/projects/MediaMaster/GameMaster/GameEntityForm/GameEntityForm.tsx
+++ app/src/components/data/projects/MediaMaster/GameMaster/GameEntityForm/GameEntityForm.tsx
@@ -5,7 +5,7 @@ import RoundButton from "../../../../../buttons/RoundButton";
 import styled from "styled-components";
 import { GameEntityType } from "../../mediaTypes/GameEntityType";
 import FormProvider from "../../../../../../hooks/useForm/FormProvider";
-import InputField from "../../../../../form/InputField";
+import EntityInputField from "../../../../../form/EntityInputField";
 import { StateType } from "../../../../../helper/types";
 
 interface GameEntityFormProps {
@@ -26,16 +26,16 @@ function GameEntityForm({
     <FormProvider state={gameState} entityClass={GameEntityType}>
       <ColumnsContainer>
         <Column colCount={3}>
-          <InputField name={"name"} state={gameState} label={"Name"} />
+          <EntityInputField name={"name"} state={gameState} label={"Name"} />
         </Column>
         <Column colCount={3}>
-          <InputField name={"name"} state={gameState} label={"Name"} />
+          <EntityInputField name={"name"} state={gameState} label={"Name"} />
         </Column>
         <Column colCount={3}>
-          <InputField name={"name"} state={gameState} label={"Name"} />
+          <EntityInputField name={"name"} state={gameState} label={"Name"} />
         </Column>
         <Column colCount={3}>
-          <InputField name={"name"} state={gameState} label={"Name"} />
+          <EntityInputField name={"name"} state={gameState} label={"Name"} />
         </Column>
       </ColumnsContainer>
       {!!handleSubmit && (
--- app/src/components/form/EntityInputField.tsx
+++ app/src/components/form/EntityInputField.tsx
@@ -1,47 +1,42 @@
-import React, {ChangeEventHandler} from "react";
+import * as React from "react";
 import styled from "styled-components";
+import {LocalStoreEntityType} from "../../hooks/useLocalStorage/LocalStoreContext";
 import {StateType} from "../helper/types";
 import {Color} from "../config/color";
 import {TEXTCOLOR} from "../config/css";
 
-interface InputFieldProps<T> {
+interface EntityInputFieldProps<T extends LocalStoreEntityType> {
   className?: string;
+  name: keyof T & string;
   state: StateType<T>;
   label?: string;
 }
 
-function InputField<T extends string | number>({
+function EntityInputField<T extends LocalStoreEntityType>({
   className,
+  name,
   state,
   label,
-}: InputFieldProps<T>) {
-  const [value, setValue] = state;
-  const onChange: ChangeEventHandler<HTMLInputElement> = (ev) => {
-    setValue(ev.target.value as T)
-  }
+}: EntityInputFieldProps<T>) {
   return (
     <div className={`${className} input-container`}>
       <span className={"input-label"}>{label + ": "}</span>
-      <input type={"text"} defaultValue={value} onChange={onChange}/>
+      <input name={name} type={"text"} defaultValue={"Test"}/>
     </div>
   );
 }
 
-export default styled(InputField)`
+export default styled(EntityInputField)`
   position: relative;
   width: 100%;
 
   .input-label {
-    display: inline-block;
-    vertical-align: middle;
     padding: 4px 0;
     margin-right: 4px;
-    width: calc(30% - 6px);
+    width: calc(30% - 4px);
   }
 
   input {
-    display: inline-block;
-    vertical-align: middle;
     padding: 6px;
     font-size: 14px;
     height: 16px;
@@ -52,4 +47,4 @@ export default styled(InputField)`
     box-shadow: 0 0 20px -5px ${Color.TEXT_PRIME_COLOR};
     ${TEXTCOLOR(Color.TEXT_PRIME_COLOR)};
   }
-` as typeof InputField;
+` as typeof EntityInputField;
